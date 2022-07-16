a2bf6cc541a687c47145d422b5deeb6d263fea3b
Lord Lothar Neumann <lotharneumann90@web.de>
2022-07-16 14:15:40 +0200

    Init commit

---
+++ app/src/main/java/de/bfw/xmluebungen/MainActivity.java
@@ -0,0 +1,29 @@
+package de.bfw.xmluebungen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+
+        Button callButton = findViewById(R.id.call_button);
+        callButton.setOnClickListener(view -> {
+            TextView callNumberInput = findViewById(R.id.call_screen_phone_number_input);
+            Toast.makeText(this, "Calling: " + callNumberInput.getText(), Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tell:" + callNumberInput.getText()));
+            startActivity(intent);
+        });
+    }
+}
